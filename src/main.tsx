ed2c3af9366fce37d659031b30838eb2d3415d81
muhammaduzairsaeed2-netizen <muhammaduzairsaeed2@gmail.com>
2026-05-09 19:16:11 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { TRPCProvider } from '@/providers/trpc'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <TRPCProvider>
+      <App />
+    </TRPCProvider>
   </BrowserRouter>,
 )
