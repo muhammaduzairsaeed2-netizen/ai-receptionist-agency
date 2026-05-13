cd9cee0778dfac52103545cc820874113bfa830c
muhammaduzairsaeed2-netizen <muhammaduzairsaeed2@gmail.com>
2026-05-13 17:15:01 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Routes, Route } from 'react-router'
-import BackToHome from './components/BackToHome'
-import Home from './pages/Home'
-import GetDemo from './pages/GetDemo'
-import DemoThanks from './pages/DemoThanks'
-import FloatingDemoButton from './components/FloatingDemoButton'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import GetDemo from './pages/GetDemo';
+import DemoThanks from './pages/DemoThanks';
+import BackToHome from './components/BackToHome';
+import FloatingDemoButton from './components/FloatingDemoButton';
 
 export default function App() {
   return (
     <>
       <BackToHome />
+      <FloatingDemoButton />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/get-demo" element={<GetDemo />} />
         <Route path="/demo-call" element={<DemoThanks />} />
       </Routes>
-      <FloatingDemoButton />
     </>
-  )
+  );
 }
