f9ffe1038147ff176cbfc96cd21b8a729b41e026
muhammaduzairsaeed2-netizen <muhammaduzairsaeed2@gmail.com>
2026-05-13 15:22:56 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
-import Demo from './pages/Demo'
+import GetDemo from './pages/GetDemo'
+import DemoThanks from './pages/DemoThanks'
+import FloatingDemoButton from './components/FloatingDemoButton'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/demo" element={<Demo />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/get-demo" element={<GetDemo />} />
+        <Route path="/demo-call" element={<DemoThanks />} />
+      </Routes>
+      <FloatingDemoButton />
+    </>
   )
 }
