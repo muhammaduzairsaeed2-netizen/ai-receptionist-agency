1135fbbead2ccf10b11b5ce2dcae2613c3fb414b
muhammaduzairsaeed2-netizen <muhammaduzairsaeed2@gmail.com>
2026-05-15 16:34:51 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,25 @@ import GetDemo from './pages/GetDemo';
 import DemoThanks from './pages/DemoThanks';
 import BackToHome from './components/BackToHome';
 import FloatingDemoButton from './components/FloatingDemoButton';
+import Terms from './pages/Terms'
+import Privacy from './pages/Privacy'
+import Cookies from './pages/Cookies'
+import GDPR from './pages/GDPR'
+import BackToHome from './components/BackToHome'
 
 export default function App() {
   return (
     <>
       <BackToHome />
-      <FloatingDemoButton />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/get-demo" element={<GetDemo />} />
         <Route path="/demo-call" element={<DemoThanks />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/cookies" element={<Cookies />} />
+        <Route path="/gdpr" element={<GDPR />} />
       </Routes>
     </>
-  );
+  )
 }
